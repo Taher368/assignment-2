6c46efa1406d8fd9ce52ddee3987d1f90b3489c6
Taher368 <taher368.personal@gmail.com>
2024-07-04 01:03:31 +0600

    product post complete

--- src/app/module/product/product.controllers.ts
+++ src/app/module/product/product.controllers.ts
@@ -4,8 +4,15 @@ import { ProductService } from "./product.services";
 const createProduct = async (req: Request, res: Response) => {
   try {
     const productData = req.body;
+    const result = await ProductService.createProductToDB(productData);
+    res.status(200).json({
+      success: true,
+      message: "Student is created successfully",
+      data: result,
+    });
   } catch (error) {
     console.log(error);
+    res.status(500).json({ success: false, message: "product created failed" });
   }
 };
 
